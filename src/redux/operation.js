3c5785ee997833c3bd244d504e7fe4ddcd64c859
Alina Demchenko <demalina88@gmail.com>
2023-08-20 09:26:21 -0700

    Update operation.js

--- src/redux/operation.js
+++ src/redux/operation.js
@@ -2,5 +2,4 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import {  fetchCars } from "../services/api";
 
 export const getCarsThunk = createAsyncThunk("cars/fetchAll", () => fetchCars());
-// export const getCarThunk = createAsyncThunk("car/fetchCar", (id) => fetchCar(id));
 
